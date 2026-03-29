e8bcb5afdccefec68d2c5cc187e39ab8cfa01bf0
AresFrappe <aariessapin05@gmail.com>
2026-03-29 21:44:23 +0800

    commit

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
+    # MongoDB connection
     MONGO_URI = os.getenv("MONGO_URI")
-    SECRET_KEY = os.getenv(
-        "SECRET_KEY", "buratsikolkaliwatkanancardiodalisaymalunggaypandesal")
+
+    # MySQL connection (must be edited to match your MySQL configuration)
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_DB = os.getenv("MYSQL_DB", "users")
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "aresDaGreatSecretKey")
