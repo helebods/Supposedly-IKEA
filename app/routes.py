68f135b6a7869bddb2b2c20df44e90f87f4cc483
CheesyChaos <jhuzteane16@gmail.com>
2026-05-02 19:36:38 +0800

    added embed in sign in

--- app/routes.py
+++ app/routes.py
@@ -154,7 +154,7 @@ def signin():
         if user:
             session["user_id"] = str(user["_id"])
 
-            if user.get("email") == "secret@ikea.com":
+            if user.get("credentials", {}).get("email") == "secret@ikea.com":
                 print("Admin user logged in:", email)
                 session["is_admin"] = True
             else:
